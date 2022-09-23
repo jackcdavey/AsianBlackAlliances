6209e117339e472e2323c5a60375570242fdb3bf
Jack Davey <johncdavey2000@gmail.com>
2022-09-22 22:38:57 -0700

    Fixed svg background wrapping in Firefox

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee536c5b018be7fc"; }
+/******/ 		__webpack_require__.h = function() { return "07cf9b2b7a56c9b5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
