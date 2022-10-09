fda97fd3b6930c21ea9c6e49a14db92936f0c4db
Jack Davey <johncdavey2000@gmail.com>
2022-10-09 04:28:02 -0700

    Added language selector

--- public/components/header.tsx
+++ public/components/header.tsx
@@ -2,6 +2,12 @@ import { Container } from '@mui/material';
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
 
 const styles = {
     banner: {
@@ -12,8 +18,9 @@ const styles = {
         height: '10vh',
         /* background: lightgreen; */
         display: 'flex',
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'flex-start',
+        justifyContent: 'flex-end',
         zIndex: 5,
         opacity: 0.95,
         backdropFilter: 'blur(10px)',
@@ -22,17 +29,42 @@ const styles = {
 }
 
 export default function Header() {
+    const [lang, setLang] = React.useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setLang(event.target.value as string);
+    };
+
     return (
         <Container style={styles.banner as React.CSSProperties} id='banner' sx={{ backgroundColor: 'primary.main' }}>
-            <a href='/' style={{ backgroundColor: 'red' }}>
-                <Image
-                    src="/media/CustomAssets/ABAlogo.png"
-                    layout='fill'
-                    objectFit='contain'
-                    alt="ABA Logo"
-                    objectPosition={'left'}
-                />
-            </a>
+            <div style={{ backgroundColor: 'red' }}>
+                <a href='/'>
+                    <Image
+                        src="/media/CustomAssets/ABAlogo.png"
+                        layout='fill'
+                        objectFit='contain'
+                        alt="ABA Logo"
+                        objectPosition={'left'}
+                    />
+                </a>
+            </div>
+            <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Language</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={lang}
+                        label="Language"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>English</MenuItem>
+
+                    </Select>
+                </FormControl>
+            </Box>
+
+
             {/* <div style={{ display: 'flex', paddingLeft: '20%' }}>
 
                 <a href='/historyOfCollaboration'>
