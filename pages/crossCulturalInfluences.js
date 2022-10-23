6e94d4b2f7a8a7b226aad805ff8a2201db764d0f
Jack Davey <johncdavey2000@gmail.com>
2022-10-22 18:39:55 -0700

    Adjusted food card padding to improve img quality

--- pages/crossCulturalInfluences.js
+++ pages/crossCulturalInfluences.js
@@ -228,7 +228,7 @@ export default function CrossCulturalInfluences({food, chef, holiday, footerCont
               <div style={styles.foodColumnBackground} > </div>
               <h2>Dishes</h2>
               {food.map((food) => (
-                <div style={{paddingBottom: "10%"}}>
+                <div style={{padding: "0 8% 8% 8%"}}>
                   <GradientMediaCard key={food._id} imgSource={urlFor(food?.image)} title={food?.title} content={food?.body} link={food?.link} useGradient={true} />
                   </div>
                ))}
